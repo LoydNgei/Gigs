506bf680cbe120d905aa0bf1e5a9309a63edfe6a
Loyd Ngei <loydngei@gmail.com>
2025-01-12 22:55:33 +0300

    added fillables

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Listing extends Model
 {
+    use HasFactory;
+    protected $fillable = ['title', 'company', 'location', 'website', 'email', 'tags', 'description', 'user_id'];
+
+
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
-    }
+    }    
 }

f41dc8c3fea52f8e8c74bf01418ca01bb7d66062
Loyd Ngei <loydngei@gmail.com>
2025-01-17 07:09:46 +0300

    Added correct references on my edit and delete buttons

--- resources/views/listings/singlelisting.blade.php
+++ resources/views/listings/singlelisting.blade.php
@@ -9,51 +9,40 @@
         <x-card class="p-10">
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-48 mr-6 mb-6" src="{{$listing->logo ? asset('storage/' .$listing->logo) : asset('/images/no-image.png')}}" alt=""/>
-
-                <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
-
+                <h3 class="text-2xl mb-2">
+                    {{$listing->title}}
+                </h3>
                 <div class="text-xl font-bold mb-4">
                     {{$listing->company}}            
                 </div>
-
                     <x-listing-tags :tagsCsv="$listing->tags"/>
-
                 <div class="text-lg my-4"><i class="fa-solid fa-location-dot"></i>
                     {{$listing->location}}
                 </div>
-
                 <div class="border border-gray-200 w-full mb-6"></div>
-
                 <div>
                     <h3 class="text-3xl font-bold mb-4">Job Description</h3>
                     <div class="text-lg space-y-6">
-
                         {{$listing->description}}
-
-                        <a href="mailto:{{$listing->email}}"
-                                class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80">
+                        <a href="mailto:{{$listing->email}}" class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80">
                                 <i class="fa-solid fa-envelope"></i>
                             Contact Employer
                         </a>
-
-                        <a href="{{$listing->website}}" target="_blank"
-                                class="block bg-black text-white py-2 rounded-xl hover:opacity-80 mt-5">
+                        <a href="{{$listing->website}}" target="_blank" class="block bg-black text-white py-2 rounded-xl hover:opacity-80 mt-5">
                                 <i class="fa-solid fa-globe"></i>
-                        Visit Website
+                            Visit Website
                         </a>
                     </div>
-
                 </div>
-
             </div>
         </x-card>
     </div>
 
     <x-card class="mt-4 p-2 flex space-x-6">
-        <a href="{{ route('listing.edit', $listing->id) }}">
+        <a href="{{ route('listing.edit', ['listing' => $listing->id]) }}">
             <i class="fa-solid fa-pencil"> Edit</i>
         </a>        
-        <form method="POST" action="{{ route('listing.destroy', $listing->id) }}">
+        <form method="POST" action="{{ route('listing.destroy', ['id' => $listing->id]) }}">
             @csrf
             @method('DELETE')
             <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>

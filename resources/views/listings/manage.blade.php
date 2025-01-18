5aac130451a475604de5038317642b87406f9de5
Loyd Ngei <loydngei@gmail.com>
2025-01-18 23:12:26 +0300

    Route corrected to best standard

--- resources/views/listings/manage.blade.php
+++ resources/views/listings/manage.blade.php
@@ -13,18 +13,18 @@
                 
                 <tr class="border-gray-300">
                     <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <a href="show.html">
+                        <a href="{{route('listing.single', ['listing' => $listing->id])}}">
                             {{$listing->title}}
                         </a>
                     </td>
                     <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <a href="/listings/{{$listing->id}}/edit" class="text-blue-400 px-6 py-2 rounded-xl"><i class="fa-solid fa-pen-to-square"></i>
+                        <a href="{{route('listing.edit', ["listing" => $listing->id])}}" class="text-blue-400 px-6 py-2 rounded-xl"><i class="fa-solid fa-pen-to-square"></i>
                             Edit
                         </a>
                     </td>
                     <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
 
-                        <form method="POST" action="{{ route('listing.destroy', ["listing" => $listing]) }}">
+                        <form method="POST" action="{{ route('listing.destroy', ["id" => $listing->id]) }}">
                             @csrf
                             @method('DELETE')
                             <button class="text-red-500"><i class="fa-solid fa-trash"></i>Delete</button>

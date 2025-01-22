f016f0ecaf0e30f33e70e7d02944460b01784963
Loyd Ngei <loydngei@gmail.com>
2025-01-22 22:13:21 +0300

    Routes file

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,14 @@ Route::post('/login', [UserController::class, 'loginpost'])->name('user.login');
 Route::post('/logout', [UserController::class, 'logout'])->name('user.logout');
 
 // Find Listings
-
-Route::get('/listings/form', [ListingController::class, 'showlistingsform'])->name('listing.form');
-Route::get('/listings/manage', [ListingController::class, 'manage']);
+Route::get('/listings', [ListingController::class, 'showlistings'])->name('listing.show');
+Route::get('/listings/form', [ListingController::class, 'showlistingsform'])->middleware('auth')->name('listing.form');
+Route::get('/listings/manage', [ListingController::class, 'manage'])->name('listing.manage');
 Route::get('/listings/{listing}', [ListingController::class, 'singlelisting'])->name('listing.single');
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->name('listing.edit');
-Route::post('/listings/form', [ListingController::class, 'createlisting'])->name('listing.create');
+Route::post('/listings/form', [ListingController::class, 'createlisting'])->middleware('auth')->name('listing.create');
 Route::put('/listings/{id}', [ListingController::class, 'update'])->name('listing.update');
 Route::delete('/listings/{id}', [ListingController::class, 'destroy'])->name('listing.destroy');
-Route::get('/listings', [ListingController::class, 'showlistings'])->name('listing.show');
+
 
 

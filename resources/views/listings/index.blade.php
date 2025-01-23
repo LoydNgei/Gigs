c0affc8fb9b414beda1c94497e511d6feb56bb0b
Loyd Ngei <loydngei@gmail.com>
2025-01-23 23:02:04 +0300

    Worked on the main index file

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,11 +1,12 @@
 <x-layout>
-    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+    <x-manual-listings></x-manual-listings>
+    {{-- <div class="relative pt-16 lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
         @unless(count($listings) == 0)
             @foreach($listings as $listing)
-                <x-listing-card :listing="$listing"/>
+                <x-listing-card :listing="$listing" />
             @endforeach
-            @else
-                <p>No listings found</p>
+        @else
+            <p>No listings found</p>
         @endunless
-    </div>
+    </div> --}}
 </x-layout>

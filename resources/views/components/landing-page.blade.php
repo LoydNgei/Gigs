94a95271ab2d78810510e4f725f2cf4763ff6de4
Loyd Ngei <loydngei@gmail.com>
2025-01-23 23:01:10 +0300

    Editing the landing page

--- resources/views/components/landing-page.blade.php
+++ resources/views/components/landing-page.blade.php
@@ -5,10 +5,9 @@
                 <div class="row align-items-center">
                     <div class="col-lg-7 col-md-6">
                         <div class="slider_text">
-                            <h5 class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".2s">4536+ Jobs listed</h5>
+                            <h5 class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".2s">100+ Jobs listed</h5>
                             <h3 class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">Find your Dream Job</h3>
-                            <p class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".4s">We provide online instant cash loans with quick approval that suit your term length</p>
-
+                            <p class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".4s">Find and post Gigs with a single click</p>
                             <div class="row mt-4"><div class="col-md-6 mb-3 mb-md-0"><div class="searching_text">
                                         <a href="{{ route('listing.show') }}" class="boxed-btn3 w-50">Browse Job</a>
                                     </div>

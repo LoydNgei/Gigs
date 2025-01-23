c7fb6dcbae6d67dbb6272dc2283eb86c04f3fd16
Loyd Ngei <loydngei@gmail.com>
2025-01-23 23:00:52 +0300

    worked on the footer file

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -11,9 +11,9 @@
                                 </a>
                             </div>
                             <p>
-                                finloan@support.com <br>
+                                Findgigs@gmail.com<br>
                                 +10 873 672 6782 <br>
-                                600/D, Green road, NewYork
+                                600/D, Green road, Nairobi
                             </p>
                             <div class="socail_links">
                                 <ul>
@@ -78,8 +78,7 @@
                                 <input type="text" placeholder="Enter your mail">
                                 <button type="submit">Subscribe</button>
                             </form>
-                            <p class="newsletter_text">Esteem spirit temper too say adieus who direct esteem esteems
-                                luckily.</p>
+                            <p class="newsletter_text">We are here to serve</p>
                         </div>
                     </div>
                 </div>
@@ -91,7 +90,7 @@
                 <div class="row">
                     <div class="col-xl-12">
                         <p class="copy_right text-center">
-                            {{-- Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> --}}
+                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
                         </p>
                     </div>
                 </div>
